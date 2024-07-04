5415de8d639eb6c6c754f284d4c460cd03f2bc2b
Lukas Brackmann <brackmann.lukas@gmail.com>
2024-07-04 11:53:03 +0200

    functional prototype

---
+++ src/ultimatetictactoe/game.py
@@ -0,0 +1,44 @@
+from .tictactoe import TicTacToe
+
+class Game():
+    def __init__(self):
+        self.active_player = 0
+        self.main_game = TicTacToe()
+        self.subgames = []
+        self.next_move = None
+        self.completed_squares = []
+        for i in range(3):
+             self.subgames.append([])
+             for j in range(3):
+                  self.subgames[i].append(TicTacToe())
+
+    def switch_player(self):
+         self.active_player = (self.active_player + 1) % 2
+
+    def play(self, i, j, k, l, player=None):
+        # only allow legal moves
+        # moves are always legal if the next move is a completed square
+        if self.next_move is not None and (i, j) not in self.completed_squares and (i, j) != self.next_move:
+            print("ILLEGAL!!!!")
+            return (None, None, None)
+        
+        if player is None:
+            player = self.active_player
+        placed = None
+        subgame_win = None
+        board_win = None
+
+        placed = self.subgames[i][j].play(k, l, player)
+        if placed is not None:
+            self.switch_player()
+            # next legal move is equivalent to current small move
+            subgame_win = self.subgames[i][j].determine_victory()
+            if subgame_win is not None:
+                self.completed_squares.append((i, j))
+                self.main_game.play(i, j, player)
+                board_win = self.main_game.determine_victory()
+            self.next_move = (k, l) if (k, l) not in self.completed_squares else None
+        return (placed, subgame_win, board_win)
+
+# singleton instance
+game = Game()
