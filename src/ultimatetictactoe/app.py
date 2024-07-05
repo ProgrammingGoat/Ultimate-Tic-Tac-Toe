13c586fae9ff47234a42cc3fb5338acd01c34e05
Lukas Brackmann <brackmann.lukas@gmail.com>
2024-07-05 11:27:22 +0200

    added new game option

--- src/ultimatetictactoe/app.py
+++ src/ultimatetictactoe/app.py
@@ -26,56 +26,44 @@ class UltimateTicTacToe(toga.App):
 
         main_box = toga.Box(style=Pack(direction=COLUMN))
 
-        # Button Handlers
-        def press_button(button: toga.Button, i, j, k, l):
-            # get legal move(s)
-            x, y = game.next_move
-
-            # catch illegal move
-            if x is not None and (i != x or j != y):
-                print("illegal from app.py")
-                return
-
-            # disable previous highlight
-            if x is not None:
-                for row in game_box.children[x].children[y].children:
-                    for style_button in row.children:
-                        style_button.style.update(background_color="transparent")
-                        style_button.refresh()
-            placed, subgame_win, board_win = game.play(i, j, k, l)
-            if placed:
-                button.text = placed
-                logger.info(f"Player {placed} played in {i}/{j} {k}/{l}")
-                logger.info(f"Grid: {game.subgames[i][j].grid}")
-                if subgame_win is not None:
-                    logger.info(f"Player {placed} wins grid {i}/{j}!")
-                    # replace tictactoe with X or O
-                    for row in game_box.children[i].children[j].children:
-                        for button in row.children:
-                            button.enabled = False
-                    if board_win is not None:
-                        logger.info(f"Player {placed} wins the main grid!")
-                        self.main_window.info_dialog(f"{board_win} won!", f"{board_win} won!")
-
-                # highlight next legal move, unless every move is legal
-                x, y = game.next_move
-                if x is not None:
-                    for row in game_box.children[x].children[y].children:
-                        for style_button in row.children:
-                            style_button.style.update(background_color="lightgreen")
-                            style_button.refresh()
-
         # Game info
         game_info_box = toga.Box()
-        game_info_label = toga.Label("It's some player's turn!", style=Pack(font_size=15, padding=10, alignment="center"))
-        game_info_box.add(game_info_label)
+        self.game_info_label = toga.Label("It's X's turn!", style=Pack(font_size=15, padding=10, alignment="center"))
+        game_info_box.add(self.game_info_label)
         main_box.add(game_info_box)
 
-        game_box = toga.Box(style=Pack(direction=COLUMN))
+        # Game Setup
+        self.game_box = toga.Box(style=Pack(direction=COLUMN))
+        self.start_game()
+        main_box.add(self.game_box)
+
+        # Main Menu
+        cmd_new_game = toga.Command(
+            self.new_game_handler,
+            text="New Game",
+            group=toga.Group.FILE
+        )
+        self.commands.add(cmd_new_game)
+
+        self.main_window = toga.MainWindow(title=self.formal_name, size=(480, 480), resizable=False)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+    async def new_game_handler(self, sender, **kwargs):
+        confirm = await self.main_window.confirm_dialog("New Game", "Are you sure? All progress will be lost!")
+        if confirm:
+            self.start_game()
+
+    def start_game(self):
+        # Clearing out any previous games
+        self.game_box.children.clear()
+        game.start()
+        self.game_info_label.text = "It's X's turn!"
+
         # Creating the large grid
         for i in range(3):
             row = toga.Box(style=Pack(direction=ROW))
-            game_box.add(row)
+            self.game_box.add(row)
 
             for j in range(3):
                 column = toga.Box(style=Pack(direction=COLUMN, padding=10))
@@ -86,7 +74,7 @@ class UltimateTicTacToe(toga.App):
                     column.add(button_row)
                     for l in range(3):
                         button = toga.Button(
-                            on_press=lambda widget, i=i, j=j, k=k, l=l: press_button(widget, i, j, k, l),
+                            on_press=lambda widget, i=i, j=j, k=k, l=l: self.grid_button_handler(widget, i, j, k, l),
                             style=Pack(width=50, 
                                        height=50, 
                                        font_size=20, 
@@ -96,11 +84,47 @@ class UltimateTicTacToe(toga.App):
                         button_row.add(button)
                 row.add(column)
 
-        main_box.add(game_box)
+    def grid_button_handler(self, button: toga.Button, i, j, k, l):
+        # get legal move(s)
+        x, y = game.next_move
 
-        self.main_window = toga.MainWindow(title=self.formal_name, size=(480, 480), resizable=False)
-        self.main_window.content = main_box
-        self.main_window.show()
+        # catch illegal move
+        if x is not None and (i != x or j != y):
+            print("illegal from app.py")
+            return
+
+        # disable previous highlight
+        if x is not None:
+            for row in self.game_box.children[x].children[y].children:
+                for style_button in row.children:
+                    style_button.style.update(background_color="transparent")
+                    style_button.refresh()
+        placed, subgame_win, board_win = game.play(i, j, k, l)
+        if placed:
+            button.text = placed
+            logger.info(f"Player {placed} played in {i}/{j} {k}/{l}")
+            logger.info(f"Grid: {game.subgames[i][j].grid}")
+            if subgame_win is not None:
+                logger.info(f"Player {placed} wins grid {i}/{j}!")
+                # replace tictactoe with X or O
+                for row in self.game_box.children[i].children[j].children:
+                    for button in row.children:
+                        button.enabled = False
+                if board_win is not None:
+                    logger.info(f"Player {placed} wins the main grid!")
+                    self.main_window.info_dialog(f"{board_win} won!", f"{board_win} won!")
+
+            # highlight next legal move, unless every move is legal
+            x, y = game.next_move
+            if x is not None:
+                for row in self.game_box.children[x].children[y].children:
+                    for style_button in row.children:
+                        style_button.style.update(background_color="lightgreen")
+                        style_button.refresh()
+            
+            # Show whose turn it is
+            next_player = "X" if game.active_player == 0 else "O"
+            self.game_info_label.text = f"It's {next_player}'s turn!"
 
 
 def main():
--- src/ultimatetictactoe/game.py
+++ src/ultimatetictactoe/game.py
@@ -1,7 +1,18 @@
 from .tictactoe import TicTacToe
 
 class Game():
-    def __init__(self):
+    # def __init__(self):
+    #     self.active_player = 0
+    #     self.main_game = TicTacToe()
+    #     self.subgames = []
+    #     self.next_move = (None, None)
+    #     self.completed_squares = []
+    #     for i in range(3):
+    #          self.subgames.append([])
+    #          for j in range(3):
+    #               self.subgames[i].append(TicTacToe())
+
+    def start(self):
         self.active_player = 0
         self.main_game = TicTacToe()
         self.subgames = []
